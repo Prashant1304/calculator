136e8c8d5eca90a3efbb561a1959f7d051ab77a7
Prashant1304 <sinha.prashant134@gmail.com>
2021-08-28 02:44:42 +0530

    first commit

--- src/component/calculater.js
+++ src/component/calculater.js
@@ -7,67 +7,83 @@ class Calculator extends React.Component {
 
         this.state = {
             value:"",
+        }
+        this.pre = {
             preValue:""
+
         }
         this.handleButton = this.handleButton.bind(this);
         // this.handlePrebutton = this.handlePrebutton.bind(this)
       }
     
 
+    //   componentDidUpdate() {
+    //     var c= this.state.preValue=this.state.value
+    //     var a =eval(c)
+    //     console.log(a)
+    //   }
 
     handleButton(e) {
-        // this.setState({preValue:(eval(this.state.preValue))})
+        // this.setState({preValue:(eval(this.state.value))});
+        
         console.log(e)
+
         if(e=="=") {
             this.setState({value:(eval(this.state.value))});
-            this.setState({preValue:""})
+            this.setState({preValue:""});
         } else if (e=="c") {
             this.setState({value:""});
         } 
-         else {
-             this.setState({value:this.state.value+e});
-            }
+        else {
+            this.setState({value:this.state.value+e});
+        }
     }
      
     render() {
-        console.log(this.state.preValue)
         return(
             <div>
                 <div className="cal-middle">
                     <div className="cal-display" >
-                       {/* {this.state.preValue} */}
+                      <p> {this.pre.preValue}</p>
                         {this.state.value}
                     </div>
                         <div className="cal-but-div">
 
-                            <div>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="c">c</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" >&#177;</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="%">%</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" >&#9003;</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="7">7</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="8">8</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="9">9</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="/">/</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="4">4</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="5">5</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="6">6</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="*">x</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="1">1</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="2">2</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="3">3</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} value="-" >-</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="0">0</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value=".">.</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="=">=</button>
-                                <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} value="+" >+</button>
-                            
+                            <div >
+                                <div className="cal-but">
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="c">c</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" >&#177;</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="%">%</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" >&#9003;</button>
+                                </div>
+                                <div className="cal-but">
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="7">7</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="8">8</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="9">9</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="/">/</button>
+                                </div>
+                                <div className="cal-but">
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="4">4</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="5">5</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="6">6</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="*">x</button>
+                                </div>
+                                <div className="cal-but">
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="1">1</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="2">2</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="3">3</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} value="-" >-</button>
+                                </div>
+                                <div className="cal-but">
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="0">0</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value=".">.</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="=">=</button>
+                                    <button className="cal-button" onClick={e => {this.handleButton(e.target.value)}} value="+" >+</button>
+                                </div>
                             </div>
 
                             <div className="cal-slide">
-                                <div onClick={this.props.handleChangeCal} style = {{paddingTop: "209px"}} >
-                                    &#x2936;
-                                </div>   
+                                <div className="cal-slide-center" onClick={this.props.handleChangeCal}> &#x2936; </div>   
                             </div>
                         
                         </div>
--- src/component/dummyPage.js
+++ src/component/dummyPage.js
@@ -6,38 +6,54 @@ class Dummy extends Component {
     render() {
         return(
             <div>
-                <div className="mother">
-                <div className="cal-slide">
-                    <div onClick={this.props.handleChangeDum} style = {{paddingTop: "209px"}} >
-                        &#x2936;
-                    </div>   
-                </div>
+                <div className="dum-middle">
+                    <div className="dum-display" >
+                    </div>
+                        <div className="dum-but-div">
 
-                <div>
-                    <button className="dum-button" type="button" value="+">mc</button>
-                    <button className="dum-button" type="button" value="*">mr</button>
-                    <button className="dum-button" type="button" value="-">m-</button>
-                    <button className="dum-button" type="button" value="/">m+</button>
-                    <button className="dum-button" type="button" value="1">sin</button>
-                    <button className="dum-button" type="button" value="2">cos</button>
-                    <button className="dum-button" type="button" value="3">tan</button>
-                    <button className="dum-button" type="button" value="4">log</button>
-                    <button className="dum-button" type="button" value="5">in</button>
-                    <button className="dum-button" type="button" value="6">(</button>
-                    <button className="dum-button" type="button" value="7">)</button>
-                    <button className="dum-button" type="button" value="8">cot</button>
-                    <button className="dum-button" type="button" value="9">sec</button>
-                    <button className="dum-button" type="button" value="0">cot</button>
-                    <button className="dum-button" type="button" value="=">c</button>
-                    <button className="dum-button" value="c" >c</button>
-                    <button className="dum-button" onClick={e => {this.handleButton(e.target.value)}} type="button" value="0">0</button>
-                    <button className="dum-button" type="button" value=".">.</button>
-                    <button className="dum-button" type="button" value="=">=</button>
-                    <button className="dum-button" value="+" >+</button>
-                            
-                </div>
+                        <div className="dum-slide">
+                                <div className="dum-slide-center" onClick={this.props.handleChangeDum} > &#x2936; </div>   
+                            </div>
                         
+
+                            <div >
+                                <div className="dum-but">
+                                    <button className="dum-button"  type="button" value="c">c</button>
+                                    <button className="dum-button"  type="button" >&#177;</button>
+                                    <button className="dum-button"  type="button" value="%">Rad</button>
+                                    <button className="dum-button"  type="button" >&#9003;</button>
+                                </div>
+                                <div className="dum-but">
+                                    <button className="dum-button"  type="button" value="7">mc</button>
+                                    <button className="dum-button"  type="button" value="8">mr</button>
+                                    <button className="dum-button"  type="button" value="9">m-</button>
+                                    <button className="dum-button"  type="button" value="/">m+</button>
+                                </div>
+                                <div className="dum-but">
+                                    <button className="dum-button"  type="button" value="4">sin</button>
+                                    <button className="dum-button"  type="button" value="5">cos</button>
+                                    <button className="dum-button"  type="button" value="6">tan</button>
+                                    <button className="dum-button"  type="button" value="*">log</button>
+                                </div>
+                                <div className="dum-but">
+                                    <button className="dum-button"  type="button" value="1">in</button>
+                                    <button className="dum-button"  type="button" value="2">#</button>
+                                    <button className="dum-button"  type="button" value="3">e</button>
+                                    <button className="dum-button"  value="-" >!</button>
+                                </div>
+                                <div className="dum-but">
+                                    <button className="dum-button"  type="button" value="0">(</button>
+                                    <button className="dum-button"  type="button" value=".">)</button>
+                                    <button className="dum-button"  type="button" value="=">$</button>
+                                    <button className="dum-button"  value="+" >*</button>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+
                 </div>
+                
             </div>
         )
     } 
